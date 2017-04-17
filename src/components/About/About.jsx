4be7af792c8a2735c2e2873d0aa6590491415c1d
dimas-lex <dimaslex@gmail.com>
2017-04-17 23:24:17 +0200

    Update readme file with github link

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,9 +3,10 @@ import React from 'react';
 const Loader = () => (
   <div className="about" >
     <header className="about__title">
-      <h1> About </h1>
+      <h1> About Author</h1>
     </header>
     <div className="about__content">
+      <h2> Struchkov Dmytro, 2017 </h2>
       <a href="https://github.com/dimas-lex"> GitHub </a>
     </div>
   </div>
