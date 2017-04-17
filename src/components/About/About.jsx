4aa8f0abccc755a4a20950f2bc317d12cbd7c4e3
dimas-lex <dimaslex@gmail.com>
2017-04-17 22:48:22 +0200

    Add Authour info component

---
+++ src/components/About/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Loader = () => (
+  <div className="about" >
+    <header className="about__title">
+      <h1> About </h1>
+    </header>
+    <div className="about__content">
+      <a href="https://github.com/dimas-lex"> GitHub </a>
+    </div>
+  </div>
+);
+
+export default Loader;
